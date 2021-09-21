b4021dd842c92d713e7476d436f76b2a5da77240
RicardoPerezLozano <90046669+RicardoPerezLozano@users.noreply.github.com>
2021-09-21 13:03:07 -0500

    Agregar archivos de proyecto.

---
+++ mod1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ActFinal1
+{
+    public partial class mod1 : Form
+    {
+        public mod1()
+        {
+            InitializeComponent();
+        }
+    }
+}
