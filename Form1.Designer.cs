d04a54bdc388ea87c683a7c5f71e48a48aad35e7
RicardoPerezLozano <90046669+RicardoPerezLozano@users.noreply.github.com>
2021-09-22 17:52:28 -0500

    creacion modulo3

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -64,6 +64,7 @@ namespace ActFinal1
             this.button3.TabIndex = 2;
             this.button3.Text = "modulo3";
             this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
